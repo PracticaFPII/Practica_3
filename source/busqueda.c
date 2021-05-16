7faa7795710a7be39edc0cc51a4898ec9c23c937
CisLo <54553754+CisLo@users.noreply.github.com>
2021-05-16 22:15:20 +0200

    16/05/2021

--- source/busqueda.c
+++ source/busqueda.c
@@ -18,13 +18,19 @@ int busqueda_ite (int vector[], int n_columnas, int num_buscado) {
     int i;
     int pos_inicial = 0;
     int pos_final = -1;
+    int posicion;
+
     i = (n_columnas-1)/2; // Buscar el medio
 
     while ((pos_inicial < pos_final) && (vector[i] != num_buscado)) {
         
-        
-        
+        if (vector[i] < num_buscado) {
+            pos_inicial = i+1;
+        } else {
+            pos_final = i-1;
+        }
 
+        i = (pos_inicial + pos_final)/2;
     }
     
 }
